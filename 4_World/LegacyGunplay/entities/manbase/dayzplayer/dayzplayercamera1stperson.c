503099d0e3f2155ddc358602a358c2cb2b94c4f3
Da0ne <38754008+Da0ne@users.noreply.github.com>
2022-04-29 02:05:30 +1000

    Update dayzplayercamera1stperson.c

--- 4_World/LegacyGunplay/entities/manbase/dayzplayer/dayzplayercamera1stperson.c
+++ 4_World/LegacyGunplay/entities/manbase/dayzplayer/dayzplayercamera1stperson.c
@@ -1,19 +1,5 @@
-modded class DayZPlayerCamera1stPerson extends DayZPlayerCameraBase
+modded class DayZPlayerCamera1stPerson 
 {
-	override void OnUpdate(float pDt, out DayZPlayerCameraResult pOutResult)
-	{
-		if (m_iBoneIndex != -1)
-		{
-			m_OffsetLS = m_pPlayer.GetBonePositionLS(m_iBoneIndex);
-		}
-		else
-		{
-			m_OffsetLS = "0 0 0";
-		}
-		
-		super.OnUpdate(pDt, pOutResult);
-	}
-
 	float GetLRAngle()
 	{
 		return m_fLeftRightAngle;
